fa915112b4c895f82b87b84f48e257fba39b9a8f
Samly Panton <smov.org@gmail.com>
2020-06-28 02:52:47 +0000

    feat: api server config

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,7 @@
 export class User {
     id: string;
-    firstname: string;
-    lastname: string;
+    first_name: string;
+    last_name: string;
     username: string;
     email: string;
     password: string;
--- src/app/server.js
+++ src/app/server.js
@@ -1,22 +1,41 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const http = require('http')
-//const mysql = require('mysql')
 //require('./passportConfig')
-//const passport = require('passport')
+const mysql = require('mysql')
+const passport = require('passport')
 const path = require('path');
-//const session = require('express-session')
+const session = require('express-session')
+//const events = require('./events')
+
+//connect to mysql
+const connection = mysql.createConnection ({
+    host : 'us-cdbr-east-05.cleardb.net',
+    port : '3306',
+    database : 'heroku_ffad0d173f051f2',
+    username : 'ba6c57ad573d67',
+    password : '0f065a41'
+})
+
+connecion.connect();
 
 const PORT = process.env.PORT || 4200;
 
-const users = [];
+const app = express()
+    .use(cors())
+    .use(bodyParser.json())
+   // .user(connection));
 
-app.use(bodyParser.json());
+
+const database = require('./db')
+const routes = require('./routes/api')
+
+const users = [];
 
 //dotenv.config();
 
-//connect to mysql
 //mysql.set('useFindAndModify', false);
 //mysql.set('useUnifiedTopology', true);
 //mysql.connect(process.env.DB_CONNECT,{useNewUrlParser: true} , () => console.log('connected to db!'));
@@ -47,9 +66,36 @@ app.use(function(req, res, next) {
 //app.get('/*', function (req, res, next)  {
 //    res.sendFile(path.join(__dirname + '/src/index.html'));
 //});
-app.get('/api/users', (req, res) => { res.json(users); });
 
-app.post('/api/user', (req, res) => { const user = req.body.user; users.push(user); res.json("user added"); });
+/* "/api/user"
+*   GET: find all users
+*   POST: creates a new user
+*
+*/
+app.get("/api/user", function(req, res) {
+
+})
+
+app.post('/api/user', function(req, res) {
+})
+
+/* "/api/user/:id"
+*   GET: find user by id
+*   PUT: update user by id
+*   DELETE: deletes user by id
+*/
+
+app.get('/api/user/:id', function(req, res) {
+})
+
+app.put('/api/user/:id', function(req, res) {
+})
+
+app.delete('/api/user/:id', function(req, res) {
+})
+//app.post('/api/users', (req, res) => { res.json(users); });
+
+//app.post('/api/user', (req, res) => { const user = req.body.user; users.push(user); res.json("user added"); });
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
