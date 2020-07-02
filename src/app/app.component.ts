f8069baeef48ab50f40667947061db64eaaf47bd
Samly Panton <smov.org@gmail.com>
2020-07-02 03:39:43 +0000

    feat: user app resource config (backend api calls)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-
-//import { AccountService } from './services/account.services';
-//import { User } from './models/user';
+import { AccountService } from './services/account.services';
+import { User } from './models/user';
 
 
 @Component({
@@ -12,11 +11,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Digits';
 
-  // user: User;
+  user: User;
 
-  // constructor(private accountService: AccountService) {
-  //   this.accountService.user.subscribe(x => this.user = x);
-  // }
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe(x => this.user = x);
+  }
 
   // logout() {
   //   this.accountService.logout();
