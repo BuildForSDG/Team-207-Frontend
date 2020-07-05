9c10726702d74f9c1213e0b1c97de8b4c2843b6f
Samly Panton <smov.org@gmail.com>
2020-07-05 03:20:42 +0000

    feat: auth.js tweak

--- src/app/auth.js
+++ src/app/auth.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../app/models/user');
+const User = require('../../src/app/models/user');
 const ctrlUser = require('./user.controller');
 const jwthelper = require('./helpers')
 
