40a63758cd6df7319252557fe153ecee5ea5bbca
Samly Panton <smov.org@gmail.com>
2020-07-05 10:44:54 +0000

    feat: auth require model config

--- src/app/auth.js
+++ src/app/auth.js
@@ -1,11 +1,9 @@
-const router = require('express').Router();
-const User = require('../models/user');
-const ctrlUser = require('./user.controller');
+const router = require('express').Router()
+const User = require('../models/user')
+const ctrlUser = require('./user.controller')
 const jwthelper = require('./helpers')
 
-
 //validation
-
 router.post('/register', ctrlUser.register);
 route.post('authenticate', ctrlUser.authenticate);
 router.get('/userProfile', jwthelper.verifyJwtToken, ctrlUser.userProfile);
