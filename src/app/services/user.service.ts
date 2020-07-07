c0550a7bf3150f1765c5f2769fa1f9959006d1f4
Samly Panton <smov.org@gmail.com>
2020-07-07 03:39:11 +0000

    feat: final push clearbad build (user.service config)

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { User } from '.././models/user';
+import { User } from '../models/user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { from } from 'rxjs';
