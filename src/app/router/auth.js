12fe1bf1c86e211ee5aab8fe17b70fda24654724
Samly Panton <smov.org@gmail.com>
2020-07-07 01:32:04 +0000

    feat: auth.js file continous build

--- src/app/router/auth.js
+++ src/app/router/auth.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const User = require('../models/user')
-const ctrlUser = require('./user.controller')
-const jwthelper = require('./helpers')
+const ctrlUser = require('../controllers/user.controller')
+const jwthelper = require('../helpers')
 
 //validation
 router.post('/register', ctrlUser.register);
-route.post('authenticate', ctrlUser.authenticate);
+route.post('/authenticate', ctrlUser.authenticate);
+//router.get('/User', User.getUserId)
 router.get('/userProfile', jwthelper.verifyJwtToken, ctrlUser.userProfile);
 
 module.exports = router;
