87ce2f1a8782d7ad1117e841c2dccc57112ce3ee
Samly Panton <smov.org@gmail.com>
2020-07-12 22:01:12 +0000

    feat: user.ts solution for model error

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,5 @@
-class User {
-    constructor (
+class User{
+    constructor(
     public id: string,
     public first_name: string,
     public last_name: string,
@@ -7,6 +7,6 @@ class User {
     public email: string,
     public password: string,
     public token: string
-    ) { }
+    ){}
 }
 export { User }
