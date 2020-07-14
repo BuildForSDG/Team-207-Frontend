e82acc893dd0cd56acc0808508ec4c5120158d7f
Samly Panton <smov.org@gmail.com>
2020-07-14 05:25:30 +0000

    feat:user.ts config tweak

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,21 +1,21 @@
 class User {
 
-    public id: string;
-    public first_name: string;
-    public last_name: string;
-    public username: string;
-    public email: string;
-    public password: string;
-    public token: string;
+    "id": string;
+    "first_name": string;
+    "last_name": string;
+    "username": string;
+    "email": string;
+    "password": string;
+    "token": string;
 
-    constructor(id: string, first_name: string, last_name: string, username: string, email: string, password: string, token: string) {
-        this.id = id;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.token = token;
-    }
+    // constructor(id: string, first_name: string, last_name: string, username: string, email: string, password: string, token: string) {
+    //     this.id = id;
+    //     this.first_name = first_name;
+    //     this.last_name = last_name;
+    //     this.username = username;
+    //     this.email = email;
+    //     this.password = password;
+    //     this.token = token;
+    // }
 }
 export { User }
