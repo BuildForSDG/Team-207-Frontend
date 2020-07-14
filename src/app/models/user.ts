ec52e91d2d77704f42e764e75bb933526e6b7050
Samly Panton <smov.org@gmail.com>
2020-07-14 22:21:25 +0000

    feat: user.ts config

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,15 @@
 class User {
 
+    constructor(id: string, first_name: string, last_name: string, username: string, email: string, password: string, token: string) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
+
     "id": string;
     "first_name": string;
     "last_name": string;
@@ -7,15 +17,5 @@ class User {
     "email": string;
     "password": string;
     "token": string;
-
-    // constructor(id: string, first_name: string, last_name: string, username: string, email: string, password: string, token: string) {
-    //     this.id = id;
-    //     this.first_name = first_name;
-    //     this.last_name = last_name;
-    //     this.username = username;
-    //     this.email = email;
-    //     this.password = password;
-    //     this.token = token;
-    // }
 }
 export { User }
