e9a1b3c435aaea0864567d9d30fd99c3fa8d8863
Samly Panton <smov.org@gmail.com>
2020-07-14 01:16:37 +0000

    chores: user.ts model construct

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,10 +1,21 @@
 class User {
-    id: string;
-    first_name: string;
-    last_name: string;
-    username: string;
-    email: string;
-    password: string;
-    token: string;
+
+    private id: string;
+    private first_name: string;
+    private last_name: string;
+    private username: string;
+    private email: string;
+    private password: string;
+    private token: string;
+
+    constructor(id: string, first_name: string, last_name: string, username: string, email: string, password: string, token: string) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.token = token;
+    }
 }
 export { User }
