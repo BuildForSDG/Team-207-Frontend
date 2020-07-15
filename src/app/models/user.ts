1f4f3a37df46df0c5a25a95b11719a769d2539d3
Samly Panton <smov.org@gmail.com>
2020-07-15 01:58:13 +0000

    feat: user.ts config

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,6 +1,6 @@
 class User {
 
-    constructor(id: string, first_name: string, last_name: string, username: string, email: string, password: string, token: string) {
+    constructor(id: string, first_name: string, last_name: string, username: string, email: string, password: string, token:string) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
