efba60616d63d40032be8b9d9854a2af91b2c001
Samly Panton <smov.org@gmail.com>
2020-07-16 02:42:23 +0000

    feat: get user and store user by ID config

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,14 +1,14 @@
 class User {
 
-    user_id: string;
-    first_name: string;
-    last_name: string;
-    username: string;
-    email: string;
-    password: string;
-    token: string;
+    user_id:string;
+    first_name:string;
+    last_name:string;
+    username:string;
+    email:string;
+    password:string;
+    token:string;
 
-    constructor(user_id: string, first_name: string, last_name: string, username: string, email: string, password: string, token:string) {
+    constructor(user_id:string, first_name:string, last_name:string, username:string, email:string, password:string, token:string) {
         this.user_id = user_id;
         this.first_name = first_name;
         this.last_name = last_name;
